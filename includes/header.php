e162a6c65a0ca9bd694b5ef5646978d9b990084d
anacarolinavoto <108465289+Kali-Gomes@users.noreply.github.com>
2022-10-07 08:55:51 -0300

    base header

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,33 @@
-<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style.css">
-            <title>Document</title>
-        </head>
-        <body>
-            
-            <header></header>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        </body>
-    </html>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href=".\assets\css\style.css">
+
+    <title> THE GAY TIMES </title>
+  </head>
+  <body>
+
+  <header>
+
+    <h1>
+        The LGBT Times
+    </h1>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Destaques</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Editoria</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Sobre</a>
+        </li>
+    </ul>
+  
+  </header>
