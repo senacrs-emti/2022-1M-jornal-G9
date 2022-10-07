959e0749ab961a0f9e7f6d6143b3792e93f5d8b3
anacarolinavoto <108465289+Kali-Gomes@users.noreply.github.com>
2022-10-07 09:59:03 -0300

    Header pronto

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href=".\assets\css\style.css">
 
-    <title> THE GAY TIMES </title>
+    <title> The Gay Times </title>
   </head>
   <body>
 
@@ -27,6 +27,9 @@
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Sobre</a>
+        </li>
+        <li class="nav-item">
+            <input type="text">
         </li>
     </ul>
   
