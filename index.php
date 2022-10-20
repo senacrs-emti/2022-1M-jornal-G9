16d7236e5db841f8b0684fa06afc8a103c7157a5
GiovanaRossi <giovanarossi.gi@gmail.com>
2022-10-20 09:01:04 -0300

    Destaques + aside
    Destaques + carrossel de bandeiras (tentativa 1)

--- index.php
+++ index.php
@@ -3,7 +3,73 @@
 ?>
 
 <main>
+<h3>DESTAQUES</h3>
+        <div class="prop">
+           <p>PROPAGANDA</p>
+        </div>
+        <div class="card mb-3" style="max-width: 860px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="https://s2.glbimg.com/aPPFcq_RGl0Wd1K-6UiFZlJlUWs=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_lgbt.png" height="100px">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Título</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 860px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="https://s2.glbimg.com/aPPFcq_RGl0Wd1K-6UiFZlJlUWs=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_lgbt.png" height="100px">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Título</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 860px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="https://s2.glbimg.com/aPPFcq_RGl0Wd1K-6UiFZlJlUWs=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_lgbt.png" height="100px">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Título</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+          </div>
 </main>
+<aside>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://pbs.twimg.com/media/ESmC75vWkAANHy5?format=jpg&name=medium" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="https://s2.glbimg.com/ubQ02O2d6oBtlp2JBxCnE4XBWB4=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_bi.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.ofuxico.com.br/wp-content/uploads/2021/06/bandeira-do-orgulho-trans-1024x683.jpg" class="d-block w-100">
+    </div>
+  </div>
+ <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+</aside>
 
 <?php
     include_once '.\includes\footer.php'
