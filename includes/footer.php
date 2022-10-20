8e8cb3e727fa05f5cdba97c3feff2d10905d2670
Sr-Pao <108465289+Kali-Gomes@users.noreply.github.com>
2022-10-20 09:00:51 -0300

    Footer e Header V1

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,47 @@
-    <footer></footer>
+    <footer class="col-12 col-lg-12">
+      
+      <div>
+        <h2>
+          The LGBT Times
+        </h2>
+        
+        <ul>
+          <li>
+            <h3> Colaboradores </h3>
+          </li>
+          <li>
+            <a href="">Adriely Rossetti</a>  
+          </li>
+          <li>
+            <a href="">Ellen Vargas</a>
+          </li>
+          <li>
+            <a href="">Giovana Rossi</a>
+          </li>
+          <li>
+            <a href="">Kali Gomes</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>
+          Redes Sociais SENAC
+        </h2>
+        <ul>
+          <li>
+            <a href="">Instagram</a>
+          </li>
+            <a href="">Twitter</a>
+            <a href="">Linkedin</a>
+        </ul>
+      </div>
+
+      <div class="col-lg-4">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Senac_logo.svg/2560px-Senac_logo.svg.png" alt="logo senac">
+      </div>
+
+    </footer>
     
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
