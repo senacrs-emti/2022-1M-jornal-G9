481b231ea71c53a2248912f288629b93a5cb3f28
Kali Gomes Santos <gomeskaliprofissional@gmail.com>
2022-10-21 10:00:43 -0300

    Att Página de noticia V1
    Co-Authored-By: Ebyyye <106695473+EllenVargas@users.noreply.github.com>

--- artigos.php
+++ artigos.php
@@ -9,17 +9,49 @@
         <img src="" alt="">
     </div>
     <section id="sectionartigos">
+
         <h1 id="h1tituloartigo">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
         <h5 id="h5subtitulo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, ea omnis veritatis, fugit tenetur mollitia obcaecati facilis unde libero vel ullam, eius numquam beatae iste et voluptates assumenda illum eum?</h3>
         <h6 id="NomeData">Por Nome Sobrenome em xx de mês xxxx, as xx:xx</h6>
-        <span>a</span>
-
-
-        
-    </section>
+        <center>
+            <img class="imgcompartilhar" src="img/compartilhar.png" alt="">
+        </center>
+        <hr>
+        <center>
+            <img class="imgartigos" src="https://oespecialista.com.br/wp-content/uploads/2021/10/super-homem-gay-superman.jpg" alt="">
+        </center>
+        <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, soluta est iusto voluptatum quas nulla recusandae, sequi optio reiciendis autem blanditiis voluptatibus itaque quibusdam commodi. Qui blanditiis nisi culpa provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quas dolorum qui, accusantium alias nemo autem. Quaerat molestias tenetur accusamus unde. Dicta eligendi reiciendis hic sint dolorem quibusdam ipsam!</p>
+        <p id="artigop">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa dolores eius distinctio maxime dicta cupiditate temporibus necessitatibus explicabo quod, nisi doloribus minus nihil aut harum voluptate aspernatur! Dicta, quidem nisi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sit laudantium deserunt repellat. Natus, officia nostrum modi impedit cumque possimus amet ab nesciunt error id ea eveniet provident, omnis nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. In earum voluptatum voluptatem odio id! Qui commodi suscipit possimus optio accusamus sapiente fugiat, officiis, earum numquam beatae tenetur, eos nesciunt</p>
+        <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aperiam deleniti quibusdam suscipit velit illum ea! Aliquid autem officia deleniti nam eos ex quia, vero alias tempora molestias! Voluptate, veniam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum vero odit aliquam sit rem ipsum itaque maiores voluptatum praesentium enim quaerat fugit iste nemo possimus, commodi, temporibus provident autem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto rem asperiores perspiciatis corrupti sed earum architecto impedit quis dolorem facere vero magni, amet, pariatur quod deleniti, commodi porro rerum.</p>
    
+    </section>
+    <section id="artigorelacionado">
+        <div id="reacionadosartigo">
+            <center><h2 id="h2relac">RELACIONADOS</h2></center>
+            <div id="relacionadoquadradoc1">
+                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
+            </div>
+            <div id="relacionadoquadradoc2">
+                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
+            </div>
+            <div id="relacionadoquadradob1">
+                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+            </div>
+            <div id="relacionadoquadradob2">
+                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+            </div>
+
+        </div>
+
+        <div id="propagandaartigo">
+            <img class="propartigo" src="https://yt3.ggpht.com/ytc/AMLnZu-EX_h6Iv0uj68spfaePgc2n7-Q8VEvGTSRzE2t=s900-c-k-c0x00ffffff-no-rj" alt="">
+        </div>
+
+    </section>
 
 
 </main>
