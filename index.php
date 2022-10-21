7167ea4d1d55a3504a013eb9946666bd750e080f
GiovanaRossi <giovanarossi.gi@gmail.com>
2022-10-21 09:11:50 -0300

    Aside + Destaques pt2
    Arrumei umas coisas e adicionei o espaço para as charges.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <div class="prop">
            <p>PROPAGANDA</p>
         </div>
-        <div class="card mb-3" style="max-width: 860px;">
+        <div class="card-mb-3" style="max-width: 860px;">
             <div class="row no-gutters">
               <div class="col-md-4">
                 <img src="https://s2.glbimg.com/aPPFcq_RGl0Wd1K-6UiFZlJlUWs=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_lgbt.png" height="100px">
@@ -20,7 +20,8 @@
               </div>
             </div>
           </div>
-          <div class="card mb-3" style="max-width: 860px;">
+        <div class="card00">
+          <div class="card-mb-3" style="max-width: 860px;">
             <div class="row no-gutters">
               <div class="col-md-4">
                 <img src="https://s2.glbimg.com/aPPFcq_RGl0Wd1K-6UiFZlJlUWs=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_lgbt.png" height="100px">
@@ -28,12 +29,13 @@
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">Título</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing alit.</p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="card mb-3" style="max-width: 860px;">
+        </div>
+          <div class="card-mb-3" style="max-width: 860px;">
             <div class="row no-gutters">
               <div class="col-md-4">
                 <img src="https://s2.glbimg.com/aPPFcq_RGl0Wd1K-6UiFZlJlUWs=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_lgbt.png" height="100px">
@@ -48,16 +50,34 @@
           </div>
 </main>
 <aside>
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carrossel" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://pbs.twimg.com/media/ESmC75vWkAANHy5?format=jpg&name=medium" class="d-block w-100">
     </div>
     <div class="carousel-item">
-      <img src="https://s2.glbimg.com/ubQ02O2d6oBtlp2JBxCnE4XBWB4=/e.glbimg.com/og/ed/f/original/2021/06/10/bandeira_bi.png" class="d-block w-100">
+      <img src="https://rcsgd.sa.ucsb.edu/images/default-source/sgd/1280px-asexual_pride_flag-svg.png?sfvrsn=71da4a93_0" class="d-block w-100">
     </div>
     <div class="carousel-item">
-      <img src="https://www.ofuxico.com.br/wp-content/uploads/2021/06/bandeira-do-orgulho-trans-1024x683.jpg" class="d-block w-100">
+      <img src="https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image11?qlt=82&wid=1024&ts=1646653117295&dpr=off&fit=constrain&fmt=png-alpha" class="d-block w-100">
+    </div>
+  </div>
+ <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+<div id="charges" class="carousel slide" data-ride="charges">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://64.media.tumblr.com/65c56e15449db2f3ac428f131bb433e2/b997982428e59682-d9/s1280x1920/c23de67a9538de4316c78c3f739d025472981726.jpg" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.otempo.com.br/image/contentid/policy:1.2536847:1630688021/ilustra-portaldobicentenario-jpeg.jpeg?f=3x2&w=1224" class="d-block w-100">
     </div>
   </div>
  <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
@@ -70,7 +90,6 @@
   </button>
 </div>
 </aside>
-
 <?php
     include_once '.\includes\footer.php'
 ?>
