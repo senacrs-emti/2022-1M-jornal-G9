884138db0029d0cf262c5e48ae705fc6422d35e1
EllenVargas <eryydilkin@gmail.com>
2022-10-21 09:51:36 -0300

    boxs do article dos assuntos

--- categorias.php
+++ categorias.php
@@ -2,9 +2,20 @@
     include_once '.\includes\header.php'
 ?>
 <link rel="stylesheet" href=".\assets\css\style.css">
+
 <main class="assuntos">
         <div>
-           <p>PROPAGANDA</p>
+           <p>BANER DO ASSUNTO</p>
+        </div>
+        <h1>POPULARES</h1>
+        <div class="box-artigos">
+            <div class="img-box"></div>
+        </div>
+        <div class="box-artigos">
+            <div class="img-box"></div>
+        </div>
+        <div class="box-artigos">
+            <div class="img-box"></div>
         </div>
 </main>
 
