ec02db3eab79e4298de45472fd3634b540e9790b
EllenVargas <eryydilkin@gmail.com>
2022-10-26 18:53:37 -0300

    pag noticia completa
    pagina base dos artigos completa

--- artigos.php
+++ artigos.php
@@ -3,11 +3,6 @@
 ?>
 
 <main>
-
-    <div id="divbanner">
-        Banner artigo
-        <img src="" alt="">
-    </div>
     <section id="sectionartigos">
 
         <h1 id="h1tituloartigo">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
@@ -18,7 +13,7 @@
         </center>
         <hr>
         <center>
-            <img class="imgartigos" src="https://oespecialista.com.br/wp-content/uploads/2021/10/super-homem-gay-superman.jpg" alt="">
+            <img class="imgartigos" src="https://i.pinimg.com/originals/47/5a/7b/475a7bb12cb9e99dd81a7b2515623412.png" alt="">
         </center>
         <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, soluta est iusto voluptatum quas nulla recusandae, sequi optio reiciendis autem blanditiis voluptatibus itaque quibusdam commodi. Qui blanditiis nisi culpa provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quas dolorum qui, accusantium alias nemo autem. Quaerat molestias tenetur accusamus unde. Dicta eligendi reiciendis hic sint dolorem quibusdam ipsam!</p>
         <p id="artigop">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa dolores eius distinctio maxime dicta cupiditate temporibus necessitatibus explicabo quod, nisi doloribus minus nihil aut harum voluptate aspernatur! Dicta, quidem nisi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sit laudantium deserunt repellat. Natus, officia nostrum modi impedit cumque possimus amet ab nesciunt error id ea eveniet provident, omnis nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. In earum voluptatum voluptatem odio id! Qui commodi suscipit possimus optio accusamus sapiente fugiat, officiis, earum numquam beatae tenetur, eos nesciunt</p>
@@ -29,28 +24,49 @@
         <div id="reacionadosartigo">
             <center><h2 id="h2relac">RELACIONADOS</h2></center>
             <div id="relacionadoquadradoc1">
-                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
             </div>
             <div id="relacionadoquadradoc2">
-                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
             </div>
             <div id="relacionadoquadradob1">
-                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
             </div>
             <div id="relacionadoquadradob2">
-                <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
             </div>
 
         </div>
 
         <div id="propagandaartigo">
-            <img class="propartigo" src="https://yt3.ggpht.com/ytc/AMLnZu-EX_h6Iv0uj68spfaePgc2n7-Q8VEvGTSRzE2t=s900-c-k-c0x00ffffff-no-rj" alt="">
+            <img class="propartigo" src="" alt="">
         </div>
+    </section>
 
+    <section id="sobre">
+        <span id="sp1">
+            <a href="">
+                <div id="vid1"></div>
+            </a>
+            <h5 class="h5sobre">NOME DO VIDEO</h5>
+        </span>
+        <span id="sp2">
+            <h2 class="h2rec">RECOMENDAÇÕES</h2>
+            <a href="">
+                <div id="vid2"></div>
+            </a>
+            <h5 class="h5sobre">NOME DO VIDEO</h5>
+        </span>
+        <span id="sp3">
+            <a href="">
+                <div id="vid3"></div>
+            </a>
+            <h5 class="h5sobre">NOME DO VIDEO</h5>
+        </span>
     </section>
 
 
