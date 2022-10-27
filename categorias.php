7b2e0943223622e22f6eccffc07972835ff3fe8d
adrielyrossetti <adrielyfragarossetti@gmail.com>
2022-10-27 09:28:28 -0300

    tentativa de ajeitar o texto da box

--- categorias.php
+++ categorias.php
@@ -4,16 +4,18 @@
 <link rel="stylesheet" href=".\assets\css\style.css">
 
 <main class="assuntos">
-        
         <h1>POPULARES</h1>
         <div class="box-artigos">
-            <div class="img-box"><img src="imgs-categoria/x.png" alt=""></div>
+            <div class="img-box"><img class="img-x" src="imgs-categoria/x.png" alt=""></div>
+            <p>lol</p>
         </div>
         <div class="box-artigos">
-            <div class="img-box"></div>
+            <div class="img-box"><img class="img-x" src="imgs-categoria/x.png" alt=""></div>
+            <p>lol</p>
         </div>
         <div class="box-artigos">
-            <div class="img-box"></div>
+            <div class="img-box"><img class="img-x" src="imgs-categoria/x.png" alt=""></div>
+            <p>lol</p>
         </div>
 </main>
 
