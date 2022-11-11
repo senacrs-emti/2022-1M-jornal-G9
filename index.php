e987d40c7646ed5aba13a119d73676742573a348
EllenVargas <106695473+EllenVargas@users.noreply.github.com>
2022-11-11 09:31:27 -0300

    home menos o aside

--- index.php
+++ index.php
@@ -5,54 +5,74 @@
   <div class="row">
 
     <main class="col-lg-8"> 
-            <div class="add-card">
-              <h1> DESTAQUES </h1>
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5rLGQQe8kxywR1sfDHYJmzjp8ImQLIbVgaA&usqp=CAU" alt="coca propaganda">
-            </div>
+      <h1>DESTAQUES</h1>
+      <section id="propagandadestaques"></section>
+      <section class="destaques">
+        <div class="imgdestaquesdiv"><img class="imgdestaque" src="" alt=""></div>
+        <div class="textosdestaque"><a href="">
+          <h4>PESSOAS NÃO ROYULADAS NA COMUNIDADE LGBT</h4>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, harum, accusamus molestias voluptates officia aliquid quia saepe dolor eaque esse asperiores architecto et amet obcaecati repellat quaerat veritatis. Iste, qui?</p>
+        </a></div>
+      </section>
 
-          <div class="card">
-            <img src="https://noticiasdatv.uol.com.br/media/_versions/artigos_2021/esdras-souza-gretchen-reproducao-instagram_fixed_large.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
+      <section class="destaques">
+        <div class="imgdestaquesdiv"><img class="imgdestaque" src="" alt=""></div>
+        <div class="textosdestaque"><a href="">
+          <h4></h4>
+          <p></p>
+        </a></div>
+      </section>
 
-          <div class="card">
-            <img src="https://noticiasdatv.uol.com.br/media/_versions/artigos_2021/esdras-souza-gretchen-reproducao-instagram_fixed_large.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-
-          <div class="card">
-            <img src="https://noticiasdatv.uol.com.br/media/_versions/artigos_2021/esdras-souza-gretchen-reproducao-instagram_fixed_large.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
+      <section class="destaques">
+        <div class="imgdestaquesdiv"><img class="imgdestaque" src="" alt=""></div>
+        <div class="textosdestaque"><a href="">
+          <h4></h4>
+          <p></p>
+        </a></div>
+      </section>
 
     </main>
-
     <aside class="col-lg-4">
 
-           
-
-        <ul class="list-group list-group-horizontal">
-          <li class="list-group-item"><img src="https://dicionariompb.com.br/wp-content/uploads/2021/04/pabllo-vittar.jpeg" alt="" style="width:100px; height:100px;"></li>
-          <li class="list-group-item"><img src="https://dicionariompb.com.br/wp-content/uploads/2021/04/pabllo-vittar.jpeg" alt="" style="width:100px; height:100px;"></li>
-        </ul>
-        <ul class="list-group list-group-horizontal">
-          <li class="list-group-item"><img src="https://dicionariompb.com.br/wp-content/uploads/2021/04/pabllo-vittar.jpeg" alt="" style="width:100px; height:100px;"></li>
-          <li class="list-group-item"><img src="https://dicionariompb.com.br/wp-content/uploads/2021/04/pabllo-vittar.jpeg" alt="" style="width:100px; height:100px;"></li>
-        </ul>
-
     </aside>
+    <section id="livrosrec">
+
+      <div class="row">
+        <center>
+        <h1>
+          RECOMENDAÇÕES
+        </h1>
+      </div>
+        </center>
+      <div class="row">
+        <div class="col-2">
+          <img class="livros" src="https://m.media-amazon.com/images/I/91yd2Mn-vAL.jpg" alt="">
+          <center><h6 class="nomeescritor">MAURENE GOO</h6></center> 
+        </div>
+        <div class="col-2">
+          <img class="livros" src="https://m.media-amazon.com/images/I/61UHNHKpj+L.jpg" alt="">
+          <center><h6 class="nomeescritor">VITOR MARTINS</h6></center> 
+        </div>
+        <div class="col-2">
+          <img class="livros" src="https://m.media-amazon.com/images/I/71+uw0YHx7L.jpg" alt="">
+          <center><h6 class="nomeescritor">CLARA ALVES</h6></center> 
+        </div>
+        <div class="col-2">
+          <img class="livros" src="https://cdl-static.s3-sa-east-1.amazonaws.com/covers/gg/9788555342066/heartstopper-de-maos-dadas-vol-4.jpg" alt="">
+          <center><h6 class="nomeescritor">ALICE OSEMAN</h6></center> 
+        </div>
+        <div class="col-2">
+          <img class="livros" src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRhYQ_1zT37H3WkqVbj4zJLjXaZ1FLWOsnzmHz1OQD3JsLuwksP5fragrKKYWWgJrRT2243mxH4LSvh0DBgk7TU00oBtJwswQrPoqwFZOg1&usqp=CAc" alt="">
+          <center><h6 class="nomeescritor">CASEY MCQUISTON</h6></center>
+        </div>
+        <div class="col-2">
+          <img class="livros" src="https://m.media-amazon.com/images/I/71SYepTUsDL.jpg" alt="">
+          <center><h6 class="nomeescritor">CASEY MCQUISTON</h6></center>
+        </div> 
+
+      </div>
+
+    </section>
   </div>
 </div>
 
