9d04e7fe4e0b830c2368bc47b0b76d47ba978ff3
Adriely <adrielyfragarossetti@gmail.com>
2022-11-18 10:06:25 -0300

    assuntos

--- categorias.php
+++ categorias.php
@@ -1,20 +1,39 @@
 <?php 
     include_once './includes/header.php'
 ?>
-<main class="assuntos">
+<main class="back">
+<article class="assuntos">
     
     <h1>POPULARES</h1>
-    <div class="box-artigos col-10">
+    <div class="box-artigos col-12">
+        
         <div class="img-box">
             <img class="img-x" src="imgs-categoria/x.png" alt="">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</p>
+        </div>
+        <p class="col-9">TITULO <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</br></p>
+    </div>
+    <div class="box-artigos col-12">
+        
+        <div class="img-box">
+            <img class="img-x" src="imgs-categoria/x.png" alt="">
+        </div>
+        <p class="col-9">TITULO <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</br></p>
+    </div>
+    <div class="box-artigos col-12">
+        
+        <div class="img-box">
+            <img class="img-x" src="imgs-categoria/x.png" alt="">
+        </div>
+        <p class="col-9">TITULO <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</br></p>
     </div>
 
-    
-</main>
-<aside>
 
+    
+</article>
+<aside>
+    <div class="propa"><p><b>Propaganda</b><p></div>
 </aside>
+</main class="back">
 <?php
     include_once '.\includes\footer.php'
 ?>
