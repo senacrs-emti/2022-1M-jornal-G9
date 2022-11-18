d20f1a16bcd2b50093bf2aa471164054d1918e26
Adriely <adrielyfragarossetti@gmail.com>
2022-11-18 08:32:26 -0300

    teste

--- categorias.php
+++ categorias.php
@@ -4,11 +4,9 @@
 <main class="assuntos">
     
     <h1>POPULARES</h1>
-    <div class="box-artigos">
+    <div class="box-artigos col-10">
         <div class="img-box">
             <img class="img-x" src="imgs-categoria/x.png" alt="">
-        </div>
-        <h3>Titulo</h3>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</p>
     </div>
 
@@ -17,3 +15,6 @@
 <aside>
 
 </aside>
+<?php
+    include_once '.\includes\footer.php'
+?>
