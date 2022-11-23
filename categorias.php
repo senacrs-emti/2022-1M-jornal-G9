5c779bae8a3038530ebc2367e7febb6711f3bc87
adrielyrossetti <adrielyfragarossetti@gmail.com>
2022-11-23 08:14:09 -0300

    categorias

--- categorias.php
+++ categorias.php
@@ -1,39 +1,87 @@
 <?php 
     include_once './includes/header.php'
 ?>
-<main class="back">
-<article class="assuntos">
-    
-    <h1>POPULARES</h1>
-    <div class="box-artigos col-12">
-        
-        <div class="img-box">
-            <img class="img-x" src="imgs-categoria/x.png" alt="">
-        </div>
-        <p class="col-9">TITULO <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</br></p>
-    </div>
-    <div class="box-artigos col-12">
-        
-        <div class="img-box">
-            <img class="img-x" src="imgs-categoria/x.png" alt="">
-        </div>
-        <p class="col-9">TITULO <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</br></p>
-    </div>
-    <div class="box-artigos col-12">
-        
-        <div class="img-box">
-            <img class="img-x" src="imgs-categoria/x.png" alt="">
-        </div>
-        <p class="col-9">TITULO <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus quibusdam magni ipsa! Obcaecati eligendi mollitia labore cum dolorem maxime magni ipsa sunt rerum quidem commodi, animi tenetur temporibus repudiandae!</br></p>
-    </div>
-  
-</article>
+<div class="container-fluid">
+  <div class="row">
 
-<aside>
-    <div class="propa"><p><b>Propaganda</b><p></div>
-</aside>
+    <main class="col-lg-8"> 
 
-</main class="back">
+      <h1>DESTAQUES</h1>
+      <section id="propagandadestaques"></section>
+      <section class="destaques">
+        <div class="imgdestaquesdiv"><img class="imgdestaque" src="" alt=""></div>
+        <div class="textosdestaque"><a href="">
+          <h4>PESSOAS NÃO ROYULADAS NA COMUNIDADE LGBT</h4>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, harum, accusamus molestias voluptates officia aliquid quia saepe dolor eaque esse asperiores architecto et amet obcaecati repellat quaerat veritatis. Iste, qui?</p>
+        </a></div>
+      </section>
+
+      <section class="destaques">
+        <div class="imgdestaquesdiv"><img class="imgdestaque" src="" alt=""></div>
+        <div class="textosdestaque"><a href="">
+          <h4></h4>
+          <p></p>
+        </a></div>
+      </section>
+
+      <section class="destaques">
+        <div class="imgdestaquesdiv"><img class="imgdestaque" src="" alt=""></div>
+        <div class="textosdestaque"><a href="">
+          <h4></h4>
+          <p></p>
+        </a></div>
+      </section>
+
+    </main>
+    <aside class="col-lg-4">
+        
+        <section id="carouselflag">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="bandeiracarimg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Gay_Pride_Flag.svg/1200px-Gay_Pride_Flag.svg.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img class="bandeiracarimg" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/screen-shot-2020-05-19-at-11-43-29-am-1589903026.png?crop=1xw:1xh;center,top&resize=480:*" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img class="bandeiracarimg" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/screen-shot-2020-05-19-at-12-03-54-pm-1589904242.png?crop=1xw:1xh;center,top&resize=480:*" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img class="bandeiracarimg" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/screen-shot-2020-05-19-at-12-18-24-pm-1589905335.png?crop=1xw:1xh;center,top&resize=480:*" class="d-block w-100" alt="">
+                </div>
+                <div class="carousel-item">
+                  <img class="bandeiracarimg" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/screen-shot-2020-05-20-at-10-51-04-am-1589986287.png?crop=1xw:1xh;center,top&resize=480:*" class="d-block w-100" alt="">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+        </section>
+
+        <section class="charge">
+          <img class="charge" src="https://conteudo.imguol.com.br/c/entretenimento/e3/2020/06/30/a-charge-do-artista-ribs-sobre-visibilidade-lgbt-1593556202835_v2_450x337.jpg" alt="">
+        </section>
+
+        <section id="artsitasaside">
+                      <ul class="list-group list-group-horizontal">
+                <li class="list-group-item"><center><img class="imgartista" src="https://veja.abril.com.br/wp-content/uploads/2017/08/3.jpg" class="grid-img" alt=""></center></li>
+                <li class="list-group-item"><center><img class="imgartista" src="https://yt3.ggpht.com/bB-BjsfmyCs27_PW0RmtI6O6Ox7_TPekQHLGgyOQDYt5nBOkvAv_rNd0xI0cIqaEAbimaqjo=s900-c-k-c0x00ffffff-no-rj" class="grid-img" alt=""></center></li>
+              </ul>
+              <ul class="list-group list-group-horizontal-sm">
+                <li class="list-group-item"><center><img class="imgartista" src="https://p2.trrsf.com/image/fget/cf/1200/1200/middle/images.terra.com/2021/09/14/905372105-design-sem-nome-2021-09-14t145125529.png" class="grid-img" alt=""></center></li>
+                <li class="list-group-item"><center><img class="imgartista" src="https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/06/35896_4B6387DB74019A97.png?w=1200&h=900&crop=1" class="grid-img" alt=""></center></li>
+              </ul>
+              
+        </section>
+
+    </aside>
 <?php
     include_once '.\includes\footer.php'
 ?>
