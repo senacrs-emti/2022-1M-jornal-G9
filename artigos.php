2ef5e09cd7c30c54fe847464548b989f6fe57976
EllenVargas <eryydilkin@gmail.com>
2022-11-25 08:18:05 -0300

    por causa da merda de uma div

--- artigos.php
+++ artigos.php
@@ -2,75 +2,79 @@
     include_once '.\includes\header.php'
 ?>
 
-<main id="mainartigos">
-    <section id="sectionartigos">
+<div class="container-fluid">
+  <div class="row">
+            <main class="col-lg-12">
+                    <section id="sectionartigos">
 
-        <h1 id="h1tituloartigo">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <h5 id="h5subtitulo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, ea omnis veritatis, fugit tenetur mollitia obcaecati facilis unde libero vel ullam, eius numquam beatae iste et voluptates assumenda illum eum?</h3>
-        <h6 id="NomeData">Por Nome Sobrenome em xx de mês xxxx, as xx:xx</h6>
-        <center>
-            <img class="imgcompartilhar" src="img/compartilhar.png" alt="">
-        </center>
-        <hr>
-        <center>
-            <img class="imgartigos" src="https://i.pinimg.com/originals/47/5a/7b/475a7bb12cb9e99dd81a7b2515623412.png" alt="">
-        </center>
-        <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, soluta est iusto voluptatum quas nulla recusandae, sequi optio reiciendis autem blanditiis voluptatibus itaque quibusdam commodi. Qui blanditiis nisi culpa provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quas dolorum qui, accusantium alias nemo autem. Quaerat molestias tenetur accusamus unde. Dicta eligendi reiciendis hic sint dolorem quibusdam ipsam!</p>
-        <p id="artigop">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa dolores eius distinctio maxime dicta cupiditate temporibus necessitatibus explicabo quod, nisi doloribus minus nihil aut harum voluptate aspernatur! Dicta, quidem nisi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sit laudantium deserunt repellat. Natus, officia nostrum modi impedit cumque possimus amet ab nesciunt error id ea eveniet provident, omnis nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. In earum voluptatum voluptatem odio id! Qui commodi suscipit possimus optio accusamus sapiente fugiat, officiis, earum numquam beatae tenetur, eos nesciunt</p>
-        <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aperiam deleniti quibusdam suscipit velit illum ea! Aliquid autem officia deleniti nam eos ex quia, vero alias tempora molestias! Voluptate, veniam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum vero odit aliquam sit rem ipsum itaque maiores voluptatum praesentium enim quaerat fugit iste nemo possimus, commodi, temporibus provident autem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto rem asperiores perspiciatis corrupti sed earum architecto impedit quis dolorem facere vero magni, amet, pariatur quod deleniti, commodi porro rerum.</p>
-   
-    </section>
-    <section id="artigorelacionado">
-        <div id="reacionadosartigo">
-            <center><h2 id="h2relac">RELACIONADOS</h2></center>
-            <div id="relacionadoquadradoc1">
-                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                        <h1 id="h1tituloartigo">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                        <h5 id="h5subtitulo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, ea omnis veritatis, fugit tenetur mollitia obcaecati facilis unde libero vel ullam, eius numquam beatae iste et voluptates assumenda illum eum?</h3>
+                        <h6 id="NomeData">Por Nome Sobrenome em xx de mês xxxx, as xx:xx</h6>
+                        <center>
+                            <img class="imgcompartilhar" src="img/compartilhar.png" alt="">
+                        </center>
+                        <hr>
+                        <center>
+                            <img class="imgartigos" src="https://i.pinimg.com/originals/47/5a/7b/475a7bb12cb9e99dd81a7b2515623412.png" alt="">
+                        </center>
+                        <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, soluta est iusto voluptatum quas nulla recusandae, sequi optio reiciendis autem blanditiis voluptatibus itaque quibusdam commodi. Qui blanditiis nisi culpa provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur temporibus quas dolorum qui, accusantium alias nemo autem. Quaerat molestias tenetur accusamus unde. Dicta eligendi reiciendis hic sint dolorem quibusdam ipsam!</p>
+                        <p id="artigop">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa dolores eius distinctio maxime dicta cupiditate temporibus necessitatibus explicabo quod, nisi doloribus minus nihil aut harum voluptate aspernatur! Dicta, quidem nisi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sit laudantium deserunt repellat. Natus, officia nostrum modi impedit cumque possimus amet ab nesciunt error id ea eveniet provident, omnis nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. In earum voluptatum voluptatem odio id! Qui commodi suscipit possimus optio accusamus sapiente fugiat, officiis, earum numquam beatae tenetur, eos nesciunt</p>
+                        <p id="artigop">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aperiam deleniti quibusdam suscipit velit illum ea! Aliquid autem officia deleniti nam eos ex quia, vero alias tempora molestias! Voluptate, veniam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum vero odit aliquam sit rem ipsum itaque maiores voluptatum praesentium enim quaerat fugit iste nemo possimus, commodi, temporibus provident autem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto rem asperiores perspiciatis corrupti sed earum architecto impedit quis dolorem facere vero magni, amet, pariatur quod deleniti, commodi porro rerum.</p>
+                
+                    </section>
+                    <section id="artigorelacionado">
+                        <div id="reacionadosartigo">
+                            <center><h2 id="h2relac">RELACIONADOS</h2></center>
+                            <div id="relacionadoquadradoc1">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
-            </div>
-            <div id="relacionadoquadradoc2">
-                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                            </div>
+                            <div id="relacionadoquadradoc2">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
-            </div>
-            <div id="relacionadoquadradob1">
-                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                            </div>
+                            <div id="relacionadoquadradob1">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
-            </div>
-            <div id="relacionadoquadradob2">
-                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+                            </div>
+                            <div id="relacionadoquadradob2">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
 
-            </div>
+                            </div>
 
-        </div>
+                        </div>
 
-        <div id="propagandaartigo">
-            <img class="propartigo" src="" alt="">
-        </div>
-    </section>
+                        <div id="propagandaartigo">
+                            <img class="propartigo" src="" alt="">
+                        </div>
+                    </section>
 
-    <section id="sobre">
-        <span id="sp1">
-            <a href="">
-                <div id="vid1"></div>
-            </a>
-            <h5 class="h5sobre">NOME DO VIDEO</h5>
-        </span>
-        <span id="sp2">
-            <h2 class="h2rec">RECOMENDAÇÕES</h2>
-            <a href="">
-                <div id="vid2"></div>
-            </a>
-            <h5 class="h5sobre">NOME DO VIDEO</h5>
-        </span>
-        <span id="sp3">
-            <a href="">
-                <div id="vid3"></div>
-            </a>
-            <h5 class="h5sobre">NOME DO VIDEO</h5>
-        </span>
-    </section>
+                    <section id="sobre">
+                        <span id="sp1">
+                            <a href="">
+                                <div id="vid1"></div>
+                            </a>
+                            <h5 class="h5sobre">NOME DO VIDEO</h5>
+                        </span>
+                        <span id="sp2">
+                            <h2 class="h2rec">RECOMENDAÇÕES</h2>
+                            <a href="">
+                                <div id="vid2"></div>
+                            </a>
+                            <h5 class="h5sobre">NOME DO VIDEO</h5>
+                        </span>
+                        <span id="sp3">
+                            <a href="">
+                                <div id="vid3"></div>
+                            </a>
+                            <h5 class="h5sobre">NOME DO VIDEO</h5>
+                        </span>
+                    </section>
 
 
-</main>
+            </main>
+    </div>
+</div>
 
 <?php
     include_once '.\includes\footer.php'
