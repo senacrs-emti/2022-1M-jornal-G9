f7549c356114affd213028c03cb51af1c7baa0db
Kali Gomes Santos <gomeskaliprofissional@gmail.com>
2022-12-01 19:30:54 -0300

    Cronicavilagay

---
+++ cronicavilagay.php
@@ -0,0 +1,80 @@
+<?php
+    include_once '.\includes\header.php'
+?>
+
+<div class="container-fluid">
+  <div class="row">
+            <main class="col-lg-12">
+                    <section id="sectionartigos">
+
+                        <h1 id="h1tituloartigo">A Vila Gay</h1>
+                        <h5 id="h5subtitulo">Um dia feliz no lugar mais feliz do mundo</h3>
+                        <h6 id="NomeData">Por Kali Gomes Donato Santos</h6>
+                        <center>
+                            <img class="imgcompartilhar" src="img/compartilhar.png" alt="">
+                        </center>
+                        <hr>
+                        <center>
+                            <img class="imgartigos" src="https://i.pinimg.com/736x/f8/f5/3d/f8f53d183d5c9fd0ba52b1442b7c4e37.jpg" alt="">
+                        </center>
+                        <p id="artigop">Recentemente, eu me mudei pra Vila Gay no Canadá, o caminho foi conturbado encontrei alguns heteros brancos cisgênero de direita conservadores me impedindo de me chegar ao meu novo lar, mi pareceire me ajudou a lidar com eles e logo o problema foi resolvido.</p>
+                        <p id="artigop">O lugar é cheio de bandeiras LGBTQIAP+ penduradas nos postes, na frente das casas e na entrada dos estabelecimentos, na frente da casa tem uma faixa de pedestre colorida. Estou muito feliz com a minha nova moradia e sei que aqui serei mais respeitade do que em qualquer outro lugar do mundo.</p>
+                        <p id="artigop">Finalizei o meu dia indo em um barzinho na esquina, estou chapando com mi parceire aqui do lado.</p>
+                    </section>
+                    <section id="artigorelacionado">
+                        <div id="reacionadosartigo">
+                            <center><h2 id="h2relac">RELACIONADOS</h2></center>
+                            <div id="relacionadoquadradoc1">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+                            </div>
+                            <div id="relacionadoquadradoc2">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+                            </div>
+                            <div id="relacionadoquadradob1">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+                            </div>
+                            <div id="relacionadoquadradob2">
+                                <h5 class="h5rel">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+                            </div>
+
+                        </div>
+
+                        <div id="propagandaartigo">
+                            <img class="propartigo" src="" alt="">
+                        </div>
+                    </section>
+
+                    <section id="sobre">
+                        <span id="sp1">
+                            <a href="">
+                                <div id="vid1"><img id="vid" src="" alt=""></div>
+                            </a>
+                            <h5 class="h5sobre"></h5>
+                        </span>
+                        <span id="sp2">
+                            <h2 class="h2rec">RECOMENDAÇÕES</h2>
+                            <a href="">
+                                <div id="vid2"><img id="vid" src="" alt=""></div>
+                            </a>
+                            <h5 class="h5sobre"></h5>
+                        </span>
+                        <span id="sp3">
+                            <a href="">
+                                <div id="vid3"><img id="vid" src="" alt=""></div>
+                            </a>
+                            <h5 class="h5sobre">COMO SURGIU "SAIR DO ARMÁRIO" - Lorelay Fox</h5>
+                        </span>
+                    </section>
+
+
+            </main>
+    </div>
+</div>
+
+<?php
+    include_once '.\includes\footer.php'
+?>
